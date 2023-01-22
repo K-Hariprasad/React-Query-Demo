36fc2f0a32ed1d65ac6e9988441dae4cf05de1fb
Hariprasad <hariprasadk125@gmail.com>
2023-01-22 14:36:42 +0530

    data fetching using axios-useEffect-useState

--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function Employee() {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  useEffect(()=>{
+    fetchData()
+  },[])
+  const fetchData = async() => {
+    try {
+        setLoading(true)
+        const url = 'http://localhost:5000/employees'
+        const res = await axios.get(url)
+        setData(res.data)
+        setLoading(false)
+    } catch (error) {
+        setError("Something went wrong!!")
+        setLoading(false)
+    }
+  }
+  if(loading){
+    return <h1>Loading.....</h1>
+  }
   return (
-    <div>Employee</div>
+    <>
+        {data?.length > 0 && data.map(i=>(
+            <section key={i.id} className="employee-card">
+                <p>{i.id}</p>
+                <p className="employee-card-name">{i.name}</p>
+                <p >{i.salary}</p>
+                <p className="employee-card-designation">{i.designation}</p>
+            </section>
+        ))}
+    </>
   )
 }
 
-export default Employee
+export default Employee;
